cee79915810f80e18294b1a44df45439251220a9
Yujie Tao <yujietao@outlook.com>
2024-06-08 15:11:44 -0700

    add

--- main.py
+++ main.py
@@ -439,7 +439,7 @@ class Segment:
         reps_f_dis = self.f_dis[self.reps][:, self.reps]
         local_max_patch_dis = np.max(reps_f_dis)
         if (
-            self.level > 2 or local_max_patch_dis / self.global_max_dis < 0.1
+            self.level > 0 or local_max_patch_dis / self.global_max_dis < 0.1
         ):  # 最多只递归一层，可调
             return
 
@@ -465,5 +465,5 @@ class Segment:
 if __name__ == "__main__":
     ply = "knife"  # 'dino' 'horse'
     mesh_model = Model(f"data/{ply}.ply")
-    Segment(mesh_model, 0).seg()
+    Segment(mesh_model, 2).seg()
     mesh_model.write_ply(f"data/{ply}-output.ply")

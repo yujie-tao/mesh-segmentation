d4a686c25e6868cf7894004a5a76eee8cdec7e6a
Yujie Tao <yujietao@outlook.com>
2024-06-08 12:16:47 -0700

    change type

--- dijkstra.pyx
+++ dijkstra.pyx
@@ -17,7 +17,7 @@ import heapq
 @cython.wraparound(False)
 @cython.cdivision(True)
 @cython.nonecheck(False)
-def dijkstra_c(int[:,:] f_nbrs_id, double[:,:] f_nbrs_dis, int[:] starts):
+def dijkstra_c(int32[:,:] f_nbrs_id, float64[:,:] f_nbrs_dis, int32[:] starts):
     # https://blog.paperspace.com/faster-numpy-array-processing-ndarray-cython/
     # 为什么numpy慢 https://stackoverflow.com/questions/42349587/cython-slow-numpy-arrays
     cdef int offset = starts[0]

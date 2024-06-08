7c78eeae19ecda30ef8e03bc59a01c9a14bf9900
Yujie Tao <yujietao@outlook.com>
2024-06-08 12:05:56 -0700

    add setup

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
-from setuptools import setup
+# from setuptools import setup
+# import numpy
+# from Cython.Build import cythonize
+
+# print(numpy.get_include())
+
+# setup(
+#     ext_modules=cythonize("dijkstra.pyx"),
+#     include_dirs=[numpy.get_include()],
+# )
+
+from setuptools import setup, Extension
 import numpy
 from Cython.Build import cythonize
 
+# Define the extension module
+extensions = [
+    Extension(
+        "dijkstra",
+        sources=["dijkstra.pyx"],
+        include_dirs=[numpy.get_include()],
+        # You can add additional libraries or directories if needed
+        # libraries=[],
+        # library_dirs=[],
+    )
+]
+
+# Setup configuration
 setup(
-    ext_modules=cythonize("dijkstra.pyx"),
-    include_dirs=[numpy.get_include()],
+    ext_modules=cythonize(extensions),
 )
